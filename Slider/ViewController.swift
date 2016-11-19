ef6a20a5b69a715c530c064b33b8fb346424840a
Minh Vu <vnm241191@gmail.com>
2016-11-19 12:38:15 -0800

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Minh Vu on 11/19/16.
+//  Copyright © 2016 Minh Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
